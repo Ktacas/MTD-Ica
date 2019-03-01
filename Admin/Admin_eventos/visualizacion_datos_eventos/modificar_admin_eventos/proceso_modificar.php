2899c2714561250cde986c27b5ee074d127c99c1
KarlxS <cj.cfaw@gmail.com>
2019-03-01 12:48:29 -0500

    Create guardar_blog.php

---
+++ Admin/Admin_eventos/visualizacion_datos_eventos/modificar_admin_eventos/proceso_modificar.php
@@ -0,0 +1,23 @@
+<?php
+    include '../../../conexion_db_blog.php';
+
+    $id=$_REQUEST['ID_titulo'];
+    $imagen=addslashes(file_get_contents($_FILES['imagen']['tmp_name']));
+    $fecha=$_POST['fecha'];
+    $titulo=$_POST['titulo'];
+    $categoria=$_POST['categoria'];
+    $descripcion=$_POST['descripcion'];
+
+    if(isset($_REQUEST['modificar'])){
+        $query="UPDATE dtBlog SET titulo='$titulo', imagen='$imagen', fecha='$fecha', categoria='$categoria', descripcion='$descripcion'
+                    WHERE ID_titulo ='$id'";
+        
+        $resultado= $conexion ->query($query);
+    
+        if($resultado){
+            header('Location: ../visualizar_blog.php');
+        }else{
+            echo '<script> alert("Error")</script>';
+        };
+    }
+?>
---
+++ Eventos/Index_eventos.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Dosis:300,400" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="main.css">
+    <link rel="stylesheet" href="../normalize.css">
+    <title>Eventos</title>
+</head>
+
+<body>
+    <div class="container">
+        <header class="header">
+            <!-- contenedor -->
+            <div class="container_header">
+                <!-- logo -->
+                <figure class="logo">
+                    <img src="../images/Blanco800x346.png" alt="logo">
+                </figure>
+                <!-- nav -->
+                <nav class="menu">
+                    <ul>
+                        <li><a href="../Principal/index.php">Inicio</a></li>
+                        <li><a href="#">Core</a></li>
+                        <li><a href="#">Eventos</a></li>
+                        <li><a href="../Blog/Index_blog.php">Blogs</a></li>
+                        <li><a href="#">Se miembro</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <section class="hero">
+            <div class="content">
+                <div class="circle-content"></div>
+                <h2 class="title-content">Lorem ipsum dolor sit amet, consectetur adip</h2>
+                <h4 class="sub-content">Lorem ipsum dolor sit amet, consectetur adip</h4>
+                <p class="date-content">Lorem ipsum dolor sit amet, consectetur adip</p>
+                <a href="" class="button-content">Ver más</a>
+            </div>
+            <figure class="event-image">
+                <img src="../images/eventos_principal1.png" alt="">
+            </figure>
+        </section>
+        <section class="miniature">
+            <a href="#" class="items">
+                <figure class="image-items">
+                    <img src="../images/g2.png" alt="">
+                </figure>
+                <span class="post-overlay">
+                    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</h3>
+                    <p>Lorem <span>ipsum dolor sit</span> amet, consectetur adipiscing elit, sed</p>
+                </span>
+            </a>
+            <a href="#" class="items">
+                <figure class="image-items">
+                    <img src="../images/g1.png" alt="">
+                </figure>
+                <span class="post-overlay">
+                    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</h3>
+                    <p>Lorem <span>ipsum dolor sit</span> amet, consectetur adipiscing elit, sed</p>
+                </span>
+            </a>
+            <a href="#" class="items">
+                <figure class="image-items">
+                    <img src="../images/g3.png" alt="">
+                </figure>
+                <span class="post-overlay">
+                    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</h3>
+                    <p>Lorem <span>ipsum dolor sit</span> amet, consectetur adipiscing elit, sed</p>
+                </span>
+            </a>
+            <a href="#" class="items">
+                <figure class="image-items">
+                    <img src="../images/g2.png" alt="">
+                </figure>
+                <span class="post-overlay">
+                    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</h3>
+                    <p>Lorem <span>ipsum dolor sit</span> amet, consectetur adipiscing elit, sed</p>
+                </span>
+            </a>
+            <a href="#" class="items">
+                <figure class="image-items">
+                    <img src="../images/g1.png" alt="">
+                </figure>
+                <span class="post-overlay">
+                    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</h3>
+                    <p>Lorem <span>ipsum dolor sit</span> amet, consectetur adipiscing elit, sed</p>
+                </span>
+            </a>
+            <a href="#" class="items">
+                <figure class="image-items">
+                    <img src="../images/g3.png" alt="">
+                </figure>
+                <span class="post-overlay">
+                    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</h3>
+                    <p>Lorem <span>ipsum dolor sit</span> amet, consectetur adipiscing elit, sed</p>
+                </span>
+            </a>
+            <a href="#" class="items">
+                <figure class="image-items">
+                    <img src="../images/g2.png" alt="">
+                </figure>
+                <span class="post-overlay">
+                    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</h3>
+                    <p>Lorem <span>ipsum dolor sit</span> amet, consectetur adipiscing elit, sed</p>
+                </span>
+            </a>
+            <a href="#" class="items">
+                <figure class="image-items">
+                    <img src="../images/g1.png" alt="">
+                </figure>
+                <span class="post-overlay">
+                    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</h3>
+                    <p>Lorem <span>ipsum dolor sit</span> amet, consectetur adipiscing elit, sed</p>
+                </span>
+            </a>
+            <a href="#" class="items">
+                <figure class="image-items">
+                    <img src="../images/g3.png" alt="">
+                </figure>
+                <span class="post-overlay">
+                    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</h3>
+                    <p>Lorem <span>ipsum dolor sit</span> amet, consectetur adipiscing elit, sed</p>
+                </span>
+            </a>
+        </section>
+        <footer class="footer">
+            <!-- logo -->
+            <figure class="logo_footer">
+                <img src="../images/Blanco800x346.png" alt="logo">
+            </figure>
+            <div class="info_footer">
+                <p>Design with 💕by
+                    <a href="#">@DanPipa</a>
+                    <a href="#">@Karlxs</a>
+                    <a href="#">@TheJeus</a>
+                    <a href="#">@Arnolín</a>
+                </p>
+            </div>
+        </footer>
+    </div>
+</body>
+
+</html>
