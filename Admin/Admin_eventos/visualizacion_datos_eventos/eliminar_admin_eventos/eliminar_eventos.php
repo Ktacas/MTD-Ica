08cbfe30fdd09fce42bfc72e5f96e72ff870b7a0
KarlxS <cj.cfaw@gmail.com>
2019-03-01 21:17:31 -0500

    Visualizar los valores de la Base de Datos

--- Admin/Admin_eventos/visualizacion_datos_eventos/eliminar_admin_eventos/eliminar_eventos.php
+++ Admin/Admin_eventos/visualizacion_datos_eventos/eliminar_admin_eventos/eliminar_eventos.php
@@ -1,14 +1,14 @@
 <?php
-    include '../../../conexion_db_blog.php';
+    include '../../conexion_db_eventos.php';
 
     $id=$_REQUEST['ID_titulo'];
 
-    $query="DELETE FROM dtBlog WHERE ID_titulo ='$id'";
+    $query="DELETE FROM `tbeventos` WHERE ID_titulo ='$id'";
         
     $resultado= $conexion ->query($query);
     
     if($resultado){
-        header('Location: ../visualizar_blog.php');
+        header('Location: ../visualizar_Eventos.php');
     }else{
         echo '<script> alert("Error")</script>';
     };
