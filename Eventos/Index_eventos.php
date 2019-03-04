8c36e3d8451de3c697a6f5e758bf3a7c28f854be
KarlxS <cj.cfaw@gmail.com>
2019-03-04 13:12:36 -0500

    el normalice

--- Eventos/Index_eventos.php
+++ Eventos/Index_eventos.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
         integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="main.css">
-    <link rel="stylesheet" href="../normalize.css">
+    <link rel="stylesheet" href="normalize.css">
     <title>Eventos</title>
 </head>
 
