5cb87843beb3e926503d203bc68950e5a4d099bd
KarlxS <cj.cfaw@gmail.com>
2019-03-04 13:10:57 -0500

    que es esto?

--- Pagina Principal/html_principal/js_principal/Pagina1_SECTION-FORMULARIO.js
+++
@@ -1,4 +0,0 @@
-const enviarFuntion = () => {
-    document.getElementById('datos').value = document.getElementById('txt').value;
-    document.getElementById('correo').value = document.getElementById('txt1').value; 
-}
