01fd1fb892104b124238258f7f1178df005e2fb3
KarlxS <cj.cfaw@gmail.com>
2019-03-17 04:32:06 -0500

    AGregando y editando admin blor crear y modificar

--- public/vistas/Admin/Admin_blog/visualizacion_datos_blog/modificar_admin_blog/modificar_blog.php
+++ public/vistas/Admin/Admin_blog/visualizacion_datos_blog/modificar_admin_blog/modificar_blog.php
@@ -4,24 +4,36 @@
     <meta charset="UTF-8">
     <title>Document</title>
     <link rel="stylesheet" href="css_admin_blog/index.css">
-
-    <style>
-        #admin_blog_container a{
-            text-decoration: none;
-            color: rgb(0, 0, 0);
-            width: 30px;
-            height: auto;
-            padding: 1px;
-            margin: 1px;
-            background-color:rgb(148, 137, 137);
-            border-style: solid;
-            border-color: rgb(116, 111, 111);
-            border-radius: 10%;
-        }
-    </style>
+    <link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Dosis:200,300,400" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../../../../css/normalize.css">
+    <link rel="stylesheet" href="../../../../../css/main_crear.css">
+    
 </head>
 
 <body>
+
+<header class="header">
+        <!-- contenedor -->
+        <div class="container_header">
+                <!-- logo -->
+                <figure class="logo">
+                    <img src="../../../../../img/Blanco800x346.png" alt="logo">
+                </figure>
+                <!-- nav -->
+                <nav class="menu">
+                    <ul>
+                        <li><a class="cool-link" href="../../principal/index.php">Inicio</a></li>
+                        <li><a class="cool-link" href="../../Core/Index_core.php">Core</a></li>
+                        <li><a class="cool-link" href="../../Eventos/Index_eventos.php">Eventos</a></li>
+                        <li><a class="cool-link" href="../../Blog/Index_blog.php">Blogs</a></li>
+                        <li><a class="button-link" href="#">Se miembro</a></li>
+                    </ul>
+                </nav>
+            </div>
+    </header>    
     <?php 
         include '../../conexion_db_blog.php';
 
@@ -36,13 +48,10 @@
 <div id="admin_blog_container">
     
     <form action="proceso_modificar.php?ID_titulo=<?php echo $row['ID_titulo']; ?>" method="POST" enctype="multipart/form-data" accept-charset="UTF-8">
-        <label>Titulo: <input type="text" REQUIRED name="titulo" placeholder="pon el titulo" value="<?php echo $row['titulo']; ?>"></label> <br><br>
-        
-        <label>Fecha: <input type="date" REQUIRED name="fecha" value="<?php echo $row['fecha']; ?>"></label> <br><br>
-        
+        <label>Titulo: <input type="text" REQUIRED name="titulo" placeholder="pon el titulo" value="<?php echo $row['titulo']; ?>"></label>
+        <label>Fecha: <input type="date" REQUIRED name="fecha" value="<?php echo $row['fecha']; ?>"></label>
+        <label>Enlace: <input type="text" REQUIRED name="enlace" placeholder="Introduce el ID" value="<?php echo $row['enlace']; ?>"></label> 
         <img src="https://img.youtube.com/vi/<?php echo $row['enlace'];?>/mqdefault.jpg" width="100" heigth="100" name="imagen" id="imagen">
-        
-
         
         <label>Categoria: <select name="categoria">
             <?php 
@@ -56,13 +65,19 @@
                     <option>C#</option><option>Xamarin</option><option selected>MTDeveloper</option>
             <?php }
             ?>
-            </select> <br><br>
+            </select> <br>
+
+            <label>Descripcion: <textarea REQUIRED name="descripcion" rows="5" cols="40" placeholder="pon la descripcion aqui" maxlength="140"x><?php echo $row['descripcion']; ?></textarea></label> <br>
         
-        <label>Descripcion: <textarea REQUIRED name="descripcion" rows="5" cols="40" placeholder="pon la descripcion aqui" maxlength="140"x><?php echo $row['descripcion']; ?></textarea></label><br><br>
         
         <input type="submit" value="modificar" name="modificar">
 
-        <a id href="visualizar_blog.php">Cancelar</a><br>
+        <a href="../visualizar_blog.php">Cancelar</a><br>
+
+        <div class="container_content">
+            <label>Contenido:</label>
+            <textarea REQUIRED name="contenido" rows="25" cols="100" placeholder="Coloca el contenido" maxlength="1100"x></textarea>
+        </div>
     </form>
 <br>
     <hr>
