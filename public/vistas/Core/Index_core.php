cc9dcebb63021d7f3e475b3f2e79c3f0d0aa58b0
Daniel Pipa <danielpipa14@gmail.com>
2019-03-17 21:05:48 -0600

    base actual

---
+++ public/vistas/Core/Index_core.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Dosis:200,300,400" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../css/normalize.css">
+    <link rel="stylesheet" href="../../css/main.css">
+    <title>Document</title>
+</head>
+<body>
+<header class="header">
+            <!-- contenedor -->
+            <div class="container_header">
+                <!-- logo -->
+                <figure class="logo">
+                    <img src="../../img/Blanco800x346.png" alt="logo">
+                </figure>
+                <!-- nav -->
+                <nav class="menu">
+                    <ul>
+                        <li><a class="cool-link" href="index.php">Inicio</a></li>
+                        <li><a class="cool-link" href="../Core/Index_core.php">Core</a></li>
+                        <li><a class="cool-link" href="../Eventos/Index_eventos.php">Eventos</a></li>
+                        <li><a class="cool-link" href="../Blog/Index_blog.php">Blogs</a></li>
+                        <li><a class="button-link" href="#">Se miembro</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+
+        <footer class="footer">
+            <!-- logo -->
+            <figure class="logo_footer">
+                <img src="../../img/Blanco800x346.png" alt="logo">
+            </figure>
+            <div class="info_footer">
+                <p>Design with 💕by
+                    <a href="#">@DanPipa</a>
+                    <a href="#">@Karlxs</a>
+                    <a href="#">@TheJeus</a>
+                    <a href="#">@Arnolín</a>
+                </p>
+            </div>
+        </footer>v
+</body>
+</html>
--- public/vistas/Principal/index_visualizacion_eventos.php
+++ public/vistas/Principal/index_visualizacion_eventos.php
@@ -9,24 +9,22 @@
 ?>
     <div class="slide">
         <div class="container_events">
-            <div class="division_events">
-            <div class="transparent"></div>
                 <!-- <div class="transparent"></div>
                 <div class="tag_events">Eventos</div>
                 
                 
                 <div class="line_events"></div> -->
+      
                 <div class="container-figure">
                     <figure class="image_events">
                         <img src="../../img/eventos_principal1.png" alt="">
                     </figure>
-                    <div class="content">
+                    <div class="content_a">
                         <h2 class="title_events"><?php echo $row['Titulo']; ?></h2>
                         <p class="description_events"><?php echo $row['Descripcion']; ?></p>
-                        <a class="bottom_events" href="../Eventos/Eventos.php?ID_titulo=<?php echo $row['ID_titulo']; ?>">Más info</a>
                     </div>
+                    <a class="bottom_events" href="../Eventos/Eventos.php?ID_titulo=<?php echo $row['ID_titulo']; ?>">Más info</a>
                 </div>
-            </div>
         </div>
     </div>
 <?php } ?>
