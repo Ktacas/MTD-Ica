c40438809d2d7d5cf22dc53679b6fc34e5070118
KarlxS <cj.cfaw@gmail.com>
2019-03-20 08:45:08 -0500

    .

---
+++ public/vistas/Admin/Admin_blog/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Dosis:200,300,400" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../../css/normalize.css">
+    <link rel="stylesheet" href="../../../css/main_a.css">
+    <title>Document</title>
+</head>
+<body>
+    <header class="header">
+        <!-- contenedor -->
+        <div class="container_header">
+                <!-- logo -->
+                <figure class="logo">
+                    <img src="../../../img/Blanco800x346.png" alt="logo">
+                </figure>
+                <!-- nav -->
+                <nav class="menu">
+                    <ul>
+                        <li><a class="cool-link" href="../../principal/index.php">Inicio</a></li>
+                        <li><a class="cool-link" href="../../Core/Index_core.php">Core</a></li>
+                        <li><a class="cool-link" href="../../Eventos/Index_eventos.php">Eventos</a></li>
+                        <li><a class="cool-link" href="../../Blog/Index_blog.php">Blogs</a></li>
+                        <li><a class="button-link" href="#">Se miembro</a></li>
+                    </ul>
+                </nav>
+            </div>
+    </header>
+    <div class="container">
+        <figure class="perfil-img">
+            <img src="../../../img/perfil_core2.jpg" alt="">
+        </figure>
+        <h1>Bienvenida <span>Daniela Galindo</span></h1>
+        <div class="links">
+            <a href="enviar_datos_blog/admin_blog_crear.php">Crear blog</a>
+            <a href="visualizacion_datos_blog/visualizar_blog.php">Visualizar blog</a>
+        </div>
+    </div>
+</body>
+</html>
